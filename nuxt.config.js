c219011b6f5167c943602297b5652c83f0021e28
mustsee <thomas.sypniewski@gmail.com>
2022-03-20 21:40:22 +0100

    Try

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,8 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
+  base: "/princesstreet/",
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "test",
