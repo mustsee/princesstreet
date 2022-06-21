46b1a725de2f23b0332751122b1c038691db200b
Thomas Sypniewski <thomas.sypniewski@gmail.com>
2022-06-21 12:23:05 +0100

    Remove prefix

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ export default {
   target: "static",
 
   router: {
-    base: "/princesstreet/",
-    //base: ""
+    //base: "/princesstreet/",
+    base: ""
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
